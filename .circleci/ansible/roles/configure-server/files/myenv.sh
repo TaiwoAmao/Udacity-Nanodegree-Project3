b180132443e7b2a4218f9aa96192b2ed45711a5e
Taiwo Amao <toamao08@outlook.com>
2023-01-07 11:51:16 +0100

    Push project3 files to remote folder

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,9 @@
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+export TYPEORM_HOST=database-2.cbez2tdfwmmu.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=postgres
+export TYPEORM_DATABASE=postgresdb
+export TYPEORM_MIGRATIONS=./src/migrations/*.ts
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
